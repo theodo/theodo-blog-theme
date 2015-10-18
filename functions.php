f4768f28ba0c708ddbe6ee5c96727733419bd5e2
nicgirault <nic.girault@gmail.com>
2015-10-18 14:48:55 +0200

    add hr in joinus shortcode

--- functions.php
+++ functions.php
@@ -1,7 +1,8 @@
 <?php
 
 function join_us_renderer( $atts ){
-	$html = "<p>You liked this article? You'd probably be a good match for our ever-growing tech team at Theodo.</p>\n";
+	$html = "<hr />";
+	$html .= "<p>You liked this article? You'd probably be a good match for our ever-growing tech team at Theodo.</p>\n";
 	$html .= "<p style=\"text-align: center;\"><a target=\"_blank\" href=\"http://www.theodo.fr/en/joinus\" class=\"button\"><i class=\"pw-icon-heart\"></i>Join Us</a></p>";
 	return $html;
 }
