ed3b552fc69e72eaf78006b6d40e610ab6e95c1e
nicgirault <nic.girault@gmail.com>
2015-11-23 13:02:14 +0100

    add share-button initialization in the footer

--- functions.php
+++ functions.php
@@ -15,7 +15,7 @@ function theodo_theme_enqueue()
 
 	wp_enqueue_script( 'jquery' );
 	wp_enqueue_script( 'share-button-js', get_theme_root_uri() . '/theodo-blog-theme/js/share-button.min.js', null, null );
-	wp_enqueue_script( 'main-theodo', get_theme_root_uri() . '/theodo-blog-theme/js/main.js', null, null );
+	wp_enqueue_script( 'main-theodo', get_theme_root_uri() . '/theodo-blog-theme/js/main.js', null, null, true );
 }
 
 add_action( 'wp_enqueue_scripts', 'theodo_theme_enqueue' );
